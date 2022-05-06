95ca76329ed0ab4a310b9032abf5aa994dac606e
Bergenroth <hannah.bergenroth@capgemini.com>
2022-05-06 10:33:07 +0200

    configure store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import App from './App'
 
 import './api/server'
 
+import store from './store'
+
+console.log('Initial state: ', store.getState())
+
+const unsubscribe = store.subscribe(() =>
+  console.log('State after dispatch: ', store.getState())
+)
+
+console.log('Dispatching action')
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+console.log('Dispatch complete')
+
+unsubscribe()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { createStore, compose, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducer from "./reducer";
+import { print1, print2, print3, loggerMiddleware } from './exampleAddons/middleware'
+import {
+  sayHiOnDispatch,
+  includeMeaningOfLife
+} from './exampleAddons/enhancers'
+
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware(print1, print2, print3, loggerMiddleware),
+  compose(sayHiOnDispatch, includeMeaningOfLife)
+)
+
+let preLoadedState
+const persistedTodosString = localStorage.getItem('todos')
+
+if (persistedTodosString) {
+  preLoadedState = {
+    todos: JSON.parse(persistedTodosString)
+  }
+}
+
+
+const store = createStore(reducer, preLoadedState, composedEnhancer)
+export default store
